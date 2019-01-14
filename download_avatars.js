76952e8c8e73fe38affc3659b86b37bb98a80481
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-14 22:37:23 +0000

    Added an empty function and called it, as per the instructions. So far, so good.

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,12 @@
 var request = require('request');
 
+function getRepoContributors (repoOwner, repoName, cb) {
+  // body...
+}
+
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+getRepoContributors('jquery', 'jquery', function(err, result){
+  console.log('Errors:', err);
+  console.log('Result:', result);
+})
