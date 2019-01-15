cd0d6ad4dc401f65884e2638a12bfd4ba54eeec2
Shmoo Ritchie <shmooritchie@gmail.com>
2019-01-15 01:19:18 +0000

    Okay! Step 7 done. I can download and save files from github :)

--- download_avatars.js
+++ download_avatars.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var secrets = require('./secrets.js');
+var fs = require('fs');
 
 function getRepoContributors (repoOwner, repoName, cb) {
   var options = {
@@ -16,14 +17,29 @@ function getRepoContributors (repoOwner, repoName, cb) {
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-getRepoContributors('jquery', 'jquery', function(err, result){
-  console.log('Errors:', err);
-  result = JSON.parse(result);
-  for (var user in result){
-    console.log('Result:', result[user].avatar_url);
-  }
-});
+// getRepoContributors('jquery', 'jquery', function(err, result){
+//   // console.log('Errors:', err);
+//   result = JSON.parse(result);
+//   for (var user in result){
+//     // console.log('Result:', result[user].avatar_url);
+//   }
+// });
 
 function downloadImageByURL (url, filePath) {
-  // body...
+  request.get(url)
+          .on('error', (err) => {
+            console.log("Something went wrong. Error," + err);
+          })
+          .on('response', (response) => {
+            console.log("We got a response!");
+          })
+          .on('end', ()=>{
+            console.log("Downloading images...");
+          })
+          .pipe(fs.createWriteStream(filePath))
+          .on('finish', ()=>{
+            console.log("Download complete. Check your directory for the file.")
+          })
 }
+
+downloadImageByURL('https://avatars2.githubusercontent.com/u/2741?v=3&s=466', 'avatars/kvirani.jpg');
